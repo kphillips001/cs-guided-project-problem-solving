5ab162a376eea9b27fc7d571b363c71a4fe8de06
Kevin <knphillips01@gmail.com>
2020-12-03 19:56:26 -0600

    problem 1 completed

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -13,8 +13,32 @@ Notes:
 - Return "invalid" if n exceeds the length of the list.
 - Return an empty list if n == 0.
 """
+# inputs/outputs
+# list of nums
+# number of items from end of list
+# outputs - list or invalid
+# edge cases - invalid if n > len of list = [] if n = 0
+
+# Plan
+# Pseudo Code
+# Plain English
+# How do we handle edge cases
+# How to handle main problem => get last n elements of list
+# [ : ] or slice => get all elements starting length - n
 
 
-def last(a, n):
+def last(arr, n):
     # Your code here
+    # Edge cases
+    if n > len(arr):
+        return 'invalid'
+    elif n == 0:
+        return []
+    # main solution
+    return arr[len(arr) - n : ] # Returns a subarray n: => represents end of array
+    # : => indicates the end of an array
 
+print(last([1, 2, 3, 4, 5], 1))
+print(last([4, 3, 9, 9, 7, 6], 3))
+print(last([1, 2, 3, 4, 5], 7) )
+print(last([1, 2, 3, 4, 5], 0))
